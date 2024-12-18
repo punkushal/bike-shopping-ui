5db7ea1ddfba650b2aa6a2c099a17c5fffca5caa
punkushal <spycymagar@gmail.com>
2024-12-18 12:26:48 +0545

    Constants File Added

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const primaryGradientColor = [
+  Color.fromARGB(255, 105, 108, 114),
+  Color(0x00000000),
+];
+
+const secondaryGradientColor = [
+  Color(0xFF3258A2),
+  Color(0xFF1F41B8),
+];

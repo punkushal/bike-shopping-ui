b641d5fb287f8fb45c0af50d0fefda83a1d4638b
punkushal <spycymagar@gmail.com>
2024-12-18 12:07:07 +0545

    GoRouter Is Used For Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
+import 'package:cycle_showcasing_ui/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _router = GoRouter(routes: [
+    GoRoute(
+      name: HomePage.routeName,
+      path: HomePage.routeName,
+      builder: (context, state) => const HomePage(),
+    ),
+  ]);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Bike Booking Ui',
+      routerConfig: _router,
       theme: ThemeData(
         useMaterial3: true,
       ),
